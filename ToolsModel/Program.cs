490caac178143208b2ea70a38c792aab11267399
PosJont <79751350+PosJont@users.noreply.github.com>
2021-10-06 17:23:44 +0800

    init

---
+++ ToolsModel/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ToolsModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("||===================================||");
+            Console.WriteLine("||===================================||");
+            Console.WriteLine("||========   USE FORK Test  =========||");
+            Console.WriteLine("||===================================||");
+            Console.WriteLine("||========   Hello C#   =============||");
+            Console.WriteLine("||===================================||");
+        }
+    }
+}
