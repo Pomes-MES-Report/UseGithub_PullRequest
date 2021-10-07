6fe9b13dbb78a46f2e5c59557f7b137577d7951e
PosJont <79751350+PosJont@users.noreply.github.com>
2021-10-07 17:24:50 +0800

    Update Program.cs

--- ToolsModel/Program.cs
+++ ToolsModel/Program.cs
@@ -4,14 +4,17 @@ namespace ToolsModel
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("||===================================||");
             Console.WriteLine("||===================================||");
-            Console.WriteLine("||========   USE FORK Test  =========||");
+            Console.WriteLine("||========   USE FORK Dev-2 =========||");
             Console.WriteLine("||===================================||");
             Console.WriteLine("||========   Hello C#   =============||");
             Console.WriteLine("||===================================||");
+            Console.WriteLine("||===================================||");
+            
         }
     }
 }
